e1ec3b58d80309055fe23fa566e2974b3be586ab
IngridNavarroA <ingrid.navarro.an@gmail.com>
2018-12-20 22:01:58 -0600

    started boat detector

--- boat-vision/detection/detector.py
+++ boat-vision/detection/detector.py
@@ -13,4 +13,20 @@ from imutils.video import FPS, VideoStream
 import time
 
 class Detector():
-	pass
+	def __init__( self, cfg, weights, class_file, conf_thresh=0.5, nms_thresh=0.4 ):
+		"""
+			Constructor
+		"""
+		self.config  = cfg
+		self.weights = weights
+		with open(class_file, 'r') as f:
+			self.classes = [line.strip() for line in f.readlines()]
+		self.conf_thresh = conf_thresh
+		self.nms_thresh = nms_thresh
+
+	def load_model( self ):
+		return cv2.dnn.readNet(self.config, self.weights)
+
+	def get_blob( self, scale ):
+		return cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+
--- boat-vision/main.py
+++ boat-vision/main.py
@@ -6,21 +6,28 @@
 	@version: 1.0
 """
 
-# from detection.detector import Detector
+from detection.detector import Detector
 import argparse
 import cv2
 
 
 ap = argparse.ArgumentParser()
-#ap.add_argument('-c', '--config', required=True, help = 'path to yolo config file')
-#ap.add_argument('-w', '--weights', required=True, help = 'path to yolo pre-trained weights')
-#ap.add_argument('-cl', '--classes', required=True, help = 'path to text file containing class names')
+ap.add_argument('-c', '--config', required=True, help = 'path to yolo config file')
+ap.add_argument('-w', '--weights', required=True, help = 'path to yolo pre-trained weights')
+ap.add_argument('-cl', '--classes', required=True, help = 'path to text file containing class names')
 ap.add_argument( '-v', '--video', required=True, help = 'path to the video' )
 
 args = ap.parse_args()
 
 
+# Initialize detector
+detector = Detector( args.config, args.weights, args.classes )
+
+(H, W) = (None, None)
+
 # Load model method
+print ( "[INFO] loading network model..." )
+net = detector.load_model()
 
 # Initialize Video Stream
 print ( "[INFO] starting video stream..." )
@@ -31,7 +38,6 @@ else: # Open specified video file
 
 # Loop over frames
 while video.isOpened():
-
 	ret, frame = video.read()
 
 	if not ret:
@@ -41,6 +47,15 @@ while video.isOpened():
 	elif cv2.waitKey ( 1 ) & 0xFF == ord ( 'q' ):
 		print ( "[INFO] quitting program...")
 		break
+
+	# Compute image blob
+	scale = 0.00392 # ?
+	blob = detector.get_blob( scale )
+	net.setInput(blob)
+
+	if W is None or H is None:
+    	H = frame.shape[0]
+    	W = frame.shape[1]
 
 	cv2.imshow ( "Frame", frame )
 
